1ac6c2db3a1169306efeca54c520bc98354499ae
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-08-05 18:31:15 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,27 @@
 import { Button } from "@/components/ui/button";
 import { handleLogin } from "./services/AuthService";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [hasToken, setHasToken] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setHasToken(true);
+  }, []);
+
   return (
     <div className="flex justify-center items-center flex-col h-[100vh] w-[100vw]">
       <h1>Welcome to Panto AI</h1>
-      {localStorage.getItem("token") ? (
+
+      {hasToken ? (
         <Link href={"/home"}>Home</Link>
       ) : (
         <div />
       )}
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 mt-4">
         <Button
           variant={"default"}
           size={"sm"}
